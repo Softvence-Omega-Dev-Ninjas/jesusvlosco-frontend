a4cd0fa1cd622d24ea3c766b4b0c25b9596a1c45
Mizanur Rahman <mizanrxb@gmail.com>
2025-08-07 22:49:01 +0600

    baseUrl added

--- src/store/api/baseApi.ts
+++ src/store/api/baseApi.ts
@@ -3,12 +3,12 @@ import type { RootState } from "../store";
 export const baseApi = createApi({
   reducerPath: "baseApi",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:5005/js",
-    // baseUrl: "https://api.lgcglobalcontractingltd.com/js",
+    // baseUrl: "http://localhost:5005/js",
+    baseUrl: "https://api.lgcglobalcontractingltd.com/js",
     prepareHeaders: (headers, { getState }) => {
-    // prepareHeaders: (headers, { getState }) => {
+      // prepareHeaders: (headers, { getState }) => {
       const token = (getState() as RootState).user.user?.accessToken || "";
-      console.log({token})
+      console.log({ token });
       if (token) {
         headers.set("Authorization", `Bearer ${token}`);
       }
