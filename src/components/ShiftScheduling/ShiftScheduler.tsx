d5786349c7a1530117e56dfd550cf501c622bf18
habibb2r <habibhk127@gmail.com>
2025-08-13 06:25:51 +0600

    updated

--- src/components/ShiftScheduling/ShiftScheduler.tsx
+++ src/components/ShiftScheduling/ShiftScheduler.tsx
@@ -1,23 +1,14 @@
-
 import { FC } from "react";
 import EmployeeAvailability from "./EmployeeAvailability";
 import WeeklyScheduleGrid from "./WeeklySchedule";
 
-
-
 const ShiftScheduler: FC = () => {
-
-
-
   return (
     <div className="flex flex-col lg:flex-row w-full h-full">
-      <EmployeeAvailability  />
-      <WeeklyScheduleGrid  />
+      <EmployeeAvailability />
+      <WeeklyScheduleGrid />
     </div>
   );
 };
 
 export default ShiftScheduler;
-
-
-
