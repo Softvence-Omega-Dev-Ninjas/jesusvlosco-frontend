438e19497cbc5f84a489f3c9be46195d4b913201
habibb2r <habibhk127@gmail.com>
2025-08-22 10:45:34 +0600

    Updated

--- src/components/ChatsComponents/ChatConversation.tsx
+++ src/components/ChatsComponents/ChatConversation.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable @typescript-eslint/no-unused-vars */
+
 import { EllipsisVertical } from "lucide-react";
 import { useEffect, useRef, useState } from "react";
 import { TChat } from "./ChatWindow";
