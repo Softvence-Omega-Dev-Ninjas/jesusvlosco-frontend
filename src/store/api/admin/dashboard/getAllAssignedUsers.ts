1e7de13e6c7511b35176b027ccaeb59951b04673
Mizanur Rahman <mizanrxb@gmail.com>
2025-08-22 16:46:36 +0600

    assigned employee fixed

--- src/store/api/admin/dashboard/getAllAssignedUsers.ts
+++ src/store/api/admin/dashboard/getAllAssignedUsers.ts
@@ -3,15 +3,11 @@ import { baseApi } from "../../baseApi";
 const getAllAssignedUsersApi = baseApi.injectEndpoints({
   endpoints: (builder) => ({
     getAllAssignedUsers: builder.query({
-      query: ({ page = 1, limit = 10, status = "DRAFT", orderBy = "asc" }) => ({
-        url: "/admin/user",
+      query: ({ orderBy = "asc" }) => ({
+        url: "/admin/user/assigned-users",
         method: "GET",
         params: {
-          page,
-          limit,
-          status,
           orderBy,
-          assigned: true, // assigned user
         },
       }),
     }),
