7302a4575f31af6aeae88f431e85cd16faa963b7
habibb2r <habibhk127@gmail.com>
2025-10-10 00:06:33 +0600

    Pull: From main

--- src/components/TimeSheets/UpdateTimeClockModal.tsx
+++ src/components/TimeSheets/UpdateTimeClockModal.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect, useState } from "react";
 import Swal from "sweetalert2";
 import { TimeSheetEntry } from "@/pages/TimeSheets";
